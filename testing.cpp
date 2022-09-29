f5ac664bafa7c8b235f1aa253c293046cb50283c
Satchit Hari <satchithari@Satchits-MacBook-Air.local>
2022-09-29 23:37:22 +0530

    Saving Progress

--- testing.cpp
+++ testing.cpp
@@ -5,12 +5,7 @@ using namespace std;
 
 int main(){
     cout<<"Hellooo!\n";
-    cout<<"Testing git\n";
-    cout<<"Testing git\n";
-    cout<<"Yay! Working...\n";
-    cout<<"Auto Save working?\n";
-    cout<<"auto run on save working?\n";
-    cout<<"auto run on save working?\n";
-    cout<<"UI changed..\n";
+    cout<<"Testing on mac\n";
     
+
 }
